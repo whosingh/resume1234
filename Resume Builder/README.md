58fc8c7c1273a6a89be71cc0cec9485bade4743c
Shanu Singh <131270105+whosingh@users.noreply.github.com>
2024-07-22 13:34:38 +0530

    Add files via upload

---
+++ Resume Builder/README.md
@@ -0,0 +1,11 @@
+# free-mern-resume-builder
+A complete resume builder mern stack application with Razorpay payment mode enabled for absolutely free.
+
+# Setps to follow
+got to root and run yarn <br />
+go to env and add your mongoose database url <br />
+go to config folder and add your firebase details <br />
+
+you are good to go now! :) <br /> 
+
+For and question ping us on Instagram:- https://www.instagram.com/codeyon_itservices/
---
+++ Resume Builder/config/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your Firebase configuration object
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
